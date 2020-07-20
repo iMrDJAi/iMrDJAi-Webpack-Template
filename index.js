0b054a5f4a82b8d9b22e9b2eec06232a8c9b6583
iMrDJAi <42304709+iMrDJAi@users.noreply.github.com>
2020-07-20 20:30:58 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+var fs = require('fs')
+var fsExtra = require('fs-extra')
+var path = require("path")
+var npm = require('npm')
+console.info('[INFO]: Webpack Template By ${Mr.DJA}!')
+console.info('[INFO]: Current Working Directory: ' + path.resolve("./"))
+fs.readdir('./', (e, files) => {
+	if (e && process.argv[2] !== "--force") {
+		console.warn('[WARN]: An Error Occurred While Trying To Check The Directory!')
+		console.error('[ERROR]: ', e)
+	} else {
+		if (process.argv[2] === "--force") {
+			console.info('[INFO]: Forcing To Bypass The Directory Check.')
+			var DirTest = true
+		} else var DirTest = files && files.length === 0
+		if (DirTest) {
+			console.info('[INFO]: Copying The Template Files...')
+			fsExtra.copy(__dirname + '/template', './', er => {
+				if (er) {
+					console.warn('[WARN]: An Error Occurred While Trying To Copy The Files!')
+					console.error('[ERROR]: ', er)
+				} else {
+					console.info('[INFO]: Installing The Project Dependencies...')
+					npm.load(err => {
+						if (err) {
+							console.warn('[WARN]: An Error Occurred While Trying To Load NPM!')
+							console.error('[ERROR]: ', err)
+							console.info('[INFO]: Try To Run \'npm i\' Manually.')
+						} else {
+							npm.on('log', message => console.log(message))
+							npm.commands.install(errr => {
+								if (errr) {
+									console.log('                                                                           ')
+									console.warn('[WARN]: An Error Occurred While Trying To Install The Dependencies!')
+									console.error('[ERROR]: ', errr)
+									console.info('[INFO]: Try To Run \'npm i\' Manually.')
+								} else {
+									console.log('                                                                           ')
+									console.info('[INFO]: Success! Enjoy :)')
+									console.info('[INFO]: Execute \'npm run start\' To Start The Dev Server, And \'npm run build\' To Build Your Project.')
+
+								}
+							})
+						}
+					})
+				}
+			})
+		} else {
+			console.warn('[WARN]: The Directory Isn\'t Empty!')
+			console.info('[INFO]: Make Sure That You\'re Running This Command Inside An Empty Directory Then Try Again!')
+			console.info('[INFO]: You Can Also Pass \'--force\' As A Parameter To The Command To Bypass That Check. DO IT AT YOUR OWN RISK!')
+		}
+	}
+})
---
+++ template/webpack.dev.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        filename: '[name].[hash].js',
+        publicPath: '/',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: "./src/index.html" })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[hash].[ext]',
+                    outputPath: 'assets'
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    "style-loader", //2. Inject styles into DOM
+                    "css-loader", //1. Turns css into commonjs
+                    { 
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()]
+                        }
+                    },
+                    { 
+                        loader: 'sass-loader',
+                        options: {
+                            webpackImporter: false,
+                            sassOptions: {
+                                includePaths: ['node_modules'],
+                            },
+                        }
+                    },
+                ]
+            }
+      ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        port: 80
+    }
+}
